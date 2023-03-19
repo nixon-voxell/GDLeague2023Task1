571656878595f10774e8f86bad6e87bebfa34720
Matt <43501106+mattlab7@users.noreply.github.com>
2023-03-19 15:25:46 +0800

    fix/refactor: level system based on previous comments

--- Assets/Scripts/LevelSystem/GridSystem.cs
+++ Assets/Scripts/LevelSystem/GridSystem.cs
@@ -5,12 +5,12 @@ public class GridSystem : MonoBehaviour
     [SerializeField] private GameObject m_Plane;
     [SerializeField] private float m_CellSize;
 
-    private int gridSizeX, gridSizeY;
-    private Vector3 origin;
-    private bool[,] grid;
+    private int m_GridSizeX, m_GridSizeY;
+    private Vector3 m_Origin;
+    private bool[,] m_Grid;
 
-    public int GridSizeX => gridSizeX;
-    public int GridSizeY => gridSizeY;
+    public int gridSizeX => m_GridSizeX;
+    public int gridSizeY => m_GridSizeY;
 
     private void Start()
     {
@@ -21,35 +21,41 @@ public class GridSystem : MonoBehaviour
     private void CalculateGridSize()
     {
         Vector3 planeSize = m_Plane.GetComponent<Renderer>().bounds.size;
-        gridSizeX = Mathf.FloorToInt(planeSize.x / m_CellSize);
-        gridSizeY = Mathf.FloorToInt(planeSize.z / m_CellSize);
-        origin = m_Plane.transform.position - planeSize / 2 + new Vector3(m_CellSize / 2, 0, m_CellSize / 2);
+        m_GridSizeX = Mathf.FloorToInt(planeSize.x / m_CellSize);
+        m_GridSizeY = Mathf.FloorToInt(planeSize.z / m_CellSize);
+        m_Origin = m_Plane.transform.position - planeSize / 2 + new Vector3(m_CellSize / 2, 0, m_CellSize / 2);
+
+        Debug.Log(m_GridSizeX);
+        Debug.Log(m_GridSizeY);
     }
 
     private void InitializeGrid()
     {
-        grid = new bool[gridSizeX, gridSizeY];
-    }
-
-    public Vector3 GetWorldPosition(int x, int y)
-    {
-        return new Vector3(x * m_CellSize, 0, y * m_CellSize) + origin + new Vector3(0, 0.5f, 0); ;
+        m_Grid = new bool[m_GridSizeX, m_GridSizeY];
     }
 
     public bool IsCellOccupied(int x, int y)
     {
-        if (x >= 0 && x < gridSizeX && y >= 0 && y < gridSizeY)
+        if (x >= 0 && x < m_GridSizeX && y >= 0 && y < m_GridSizeY)
         {
-            return grid[x, y];
+            return m_Grid[x, y];
         }
         return true;
     }
 
     public void SetCellState(int x, int y, bool isOccupied)
     {
-        if (x >= 0 && x < gridSizeX && y >= 0 && y < gridSizeY)
+        if (x >= 0 && x < m_GridSizeX && y >= 0 && y < m_GridSizeY)
         {
-            grid[x, y] = isOccupied;
+            m_Grid[x, y] = isOccupied;
         }
+    }
+
+    public Vector2Int WorldToGridPosition(Vector3 worldPosition)
+    {
+        int x = Mathf.FloorToInt((worldPosition.x - m_Origin.x) / m_CellSize);
+        int y = Mathf.FloorToInt((worldPosition.z - m_Origin.z) / m_CellSize);
+
+        return new Vector2Int(x, y);
     }
 }
