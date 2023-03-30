7b3ef791c028917ad178d82c2b7d5944e5ba207d
Kudoshi <brendenguan@live.com>
2023-03-30 09:47:09 +0800

    Added Remaining Unimplemented Skill Class

---
+++ Assets/Scripts/Skill/GauntletSkill.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GauntletSkill", menuName = "ScriptableObjects/Gauntlet Skill")]
+public class GauntletSkill : AbstractSkill
+{
+
+}
