0d37bb8f9d6283cf639403453a3508f87f675755
Nixon <voxell.technologies@gmail.com>
2023-03-30 13:05:16 +0800

    add DamageRoutine to KnifeSkill

--- Assets/Scripts/Skill/KnifeSkill.cs
+++ Assets/Scripts/Skill/KnifeSkill.cs
@@ -19,12 +19,54 @@ public class KnifeSkill : AbstractSkill
         VisualEffect vfx = levelManager.VisualEffectPool.GetNextObject();
 
         Transform playerTrans = player.transform;
-        vfx.transform.SetPositionAndRotation(playerTrans.position, playerTrans.rotation);
+        Vector3 eulerAngle = playerTrans.rotation.eulerAngles;
+        eulerAngle.x = 90.0f;
+        vfx.transform.rotation = Quaternion.Euler(eulerAngle);
         vfx.enabled = true;
         vfx.visualEffectAsset = this.CastFX;
         vfx.Play();
 
+        player.StartCoroutine(this.DamageRoutine(player, vfx));
         player.StartCoroutine(this.CleanupRoutine(player, vfx));
+    }
+
+    private IEnumerator DamageRoutine(Player player, VisualEffect vfx)
+    {
+        Transform playerTrans = player.transform;
+        float startTime = Time.time;
+        bool damaged = false;
+
+        while (Time.time - startTime < this.CastTime)
+        {
+            vfx.transform.position = playerTrans.position;
+            // just move the position if damage has been done
+            if (!damaged)
+            {
+                // check if it hits anything
+                RaycastHit hit;
+                if (Physics.SphereCast(
+                    playerTrans.position + this.PositionOffset,
+                    this.Radius, playerTrans.forward,
+                    out hit, this.Range
+                )) {
+                    Player otherPlayer = hit.collider.GetComponent<Player>();
+                    if (otherPlayer != null && otherPlayer != player)
+                    {
+                        otherPlayer.Damage(this.Damage);
+                        damaged = true;
+                    }
+
+                    DestructableObstacle obstacle = hit.collider.GetComponent<DestructableObstacle>();
+                    if (obstacle != null)
+                    {
+                        obstacle.DestroyObstacle();
+                        damaged = true;
+                    }
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     private IEnumerator CleanupRoutine(Player player, VisualEffect vfx)
@@ -37,28 +79,5 @@ public class KnifeSkill : AbstractSkill
         vfx.Stop();
         vfx.enabled = false;
         vfx.visualEffectAsset = null;
-
-        Transform playerTrans = player.transform;
-        // check if it hits anything
-        RaycastHit hit;
-        if (Physics.SphereCast(
-            playerTrans.position + this.PositionOffset,
-            this.Radius, playerTrans.forward,
-            out hit, this.Range
-        )) {
-            Player otherPlayer = hit.collider.GetComponent<Player>();
-            if (otherPlayer != null && otherPlayer != player)
-            {
-                otherPlayer.Damage(this.Damage);
-                yield break;
-            }
-
-            DestructableObstacle obstacle = hit.collider.GetComponent<DestructableObstacle>();
-            if (obstacle != null)
-            {
-                obstacle.DestroyObstacle();
-                yield break;
-            }
-        }
     }
 }
